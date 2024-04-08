449e4ca6790307badc4f208b806a42cdfef7e291
Zaar Hai <haizaar@haizaar.com>
2024-04-08 18:30:54 +1000

    This classifier always classified something

--- mediapipe_gesture_classifier/gesture_classifier.py
+++ mediapipe_gesture_classifier/gesture_classifier.py
@@ -1,6 +1,5 @@
 import copy
 import itertools
-from typing import Optional
 from enum import Enum
 from pathlib import Path
 
@@ -35,7 +34,7 @@ class GestureClassifier(object):
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-    def classify(self, landmark_list) -> tuple[Optional[Gesture], Confidence]:
+    def classify(self, landmark_list) -> tuple[Gesture, Confidence]:
         input_details_tensor_index = self.input_details[0]['index']
         self.interpreter.set_tensor(
             input_details_tensor_index,
